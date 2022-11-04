14ab46165e3d8ef3f0c68b1b54a7a816c881202f
tschneit717 <tschneit717@gmail.com>
2022-11-04 15:23:16 -0400

    Discovery and pseudo coding

---
+++ src/main/java/com/schneit/parksproject/DirectionsController.java
@@ -0,0 +1,12 @@
+package com.schneit.parksproject;
+
+public class DirectionsController {
+    // Create an endpoint to get directions
+    // Take user input and create a query
+    // Return itinerary steps
+    // TODO: Allow for chaining queries to show directions to multiple locations
+    // Ideas:
+    // Save history into database
+    // Find most convenient route between queries
+    //
+}
